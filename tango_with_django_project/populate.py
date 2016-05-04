d7b161ff11e34e3a3f359d821a3b543faa204115
bretuobay <bretuobay@gmail.com>
2016-05-04 17:43:31 +0200

    added extra migrations

--- tango_with_django_project/populate.py
+++ tango_with_django_project/populate.py
@@ -9,7 +9,7 @@ from rango.models import Category, Page
 
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python',128,32)
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
@@ -23,7 +23,7 @@ def populate():
         title="Learn Python in 10 Minutes",
         url="http://www.korokithakis.net/tutorials/python/")
 
-    django_cat = add_cat("Django")
+    django_cat = add_cat("Django",299,12)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
@@ -37,7 +37,7 @@ def populate():
         title="How to Tango with Django",
         url="http://www.tangowithdjango.com/")
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks",232,445)
 
     add_page(cat=frame_cat,
         title="Bottle",
@@ -59,8 +59,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name,views,likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     return c
 
 # Start execution here!
