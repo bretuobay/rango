4ddba1ac13dbbd87506a6c12ef8451bccdd252a4
bretuobay <bretuobay@gmail.com>
2016-05-10 18:01:02 +0200

    almost done

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+
+#create new class to redirect to index Page
+class MyRegistrationView(RegistrationView):
+    def get_sucess_url(self,request,user):
+        return '/rango/'
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,6 +16,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/register/$',MyRegistrationView.as_view(),name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 )
 
